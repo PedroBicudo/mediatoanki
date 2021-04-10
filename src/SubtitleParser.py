62aa6793087b1f4631def5e6ff55fca840c70bee
Pedro <43938917+PedroBicudo@users.noreply.github.com>
2021-04-09 23:16:00 -0300

    Adicionar exceptions a classe subtitleparser. (#4)
    Co-authored-by: Pedro Henrique <pedroabicudo_2012@hotmail.com>

--- src/SubtitleParser.py
+++ src/SubtitleParser.py
@@ -11,8 +11,8 @@ class SubtitleParser:
 
     def get_subtitles_from_file(self, filename) -> List[Subtitle]:
         file_format = SubtitleParser._get_file_format(filename).lower()
-        subtitle_regex = SubtitleParser._get_regex_based_on_file_format(file_format)
-        file = open(filename, "r")
+        subtitle_regex = self._get_regex_based_on_file_format(file_format)
+        file = self._get_subtitle_file(filename)
         subs = []
         for line in file.readlines():
             match = re.match(subtitle_regex.REGEX_LINE, line)
@@ -30,6 +30,16 @@ class SubtitleParser:
                 subs[-1].text += f"{line.strip()} "
 
         return subs
+
+    def _get_subtitle_file(self, filename):
+        try:
+            return self._open_file_on_reading_mode(filename)
+
+        except Exception as error:
+            raise Exception("Não foi possível abrir o arquivo de legendas.")
+
+    def _open_file_on_reading_mode(self, filename):
+        return open(filename, 'r')
 
     @staticmethod
     def _get_timedelta_from(time: str) -> timedelta:
@@ -55,12 +65,18 @@ class SubtitleParser:
     def _has_hour_field(time: str):
         return time.count(":") == 2
 
-    @staticmethod
-    def _get_regex_based_on_file_format(file_format) -> SubtitleFormat:
-        return {
-            "vtt": Vtt
+    def _get_regex_based_on_file_format(self, file_format) -> SubtitleFormat:
+        try:
+            return SubtitleParser._get_subtitle_regex(file_format)
+        except Exception as _:
+            raise NotImplementedError(f"O formato '{file_format}' não está disponível")
 
-        }.get(file_format, Vtt)
+    @staticmethod
+    def _get_subtitle_regex(file_format) -> SubtitleFormat:
+        subs_regex = {
+            "vtt": Vtt
+        }
+        return subs_regex[file_format]
 
     @staticmethod
     def _get_file_format(filename):
