b375e87bf061b69397c9f653339eda2d99521693
Pedro <43938917+PedroBicudo@users.noreply.github.com>
2021-05-01 11:17:13 -0300

    Deep copy agora é funcional. (#11)
    Co-authored-by: Pedro Henrique <pedroabicudo_2012@hotmail.com>

--- src/utils/CopyUtils.py
+++ src/utils/CopyUtils.py
@@ -5,13 +5,14 @@ from src.model.Subtitle import Subtitle
 def deep_copy_of_subs(subtitles: List[Subtitle]) -> List[Subtitle]:
     subtitles_copied = []
     for subtitle in subtitles:
-        subtitles_copied.append(subtitle)
+        subtitles_copied.append(_instance_subtitle(subtitle))
 
     return subtitles_copied
 
 
 def _instance_subtitle(subtitle_old: Subtitle) -> Subtitle:
     subtitle_new = Subtitle(subtitle_old.time_start, subtitle_old.time_end)
+    subtitle_new.text = subtitle_old.text
     subtitle_new.audio = subtitle_old.audio
     subtitle_new.frame = subtitle_old.frame
 
