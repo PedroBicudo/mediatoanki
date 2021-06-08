e6898bae6938994189b72f56e9071343d3d57760
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2021-06-08 14:31:47 -0300

    Métodos do CopyUtils remodelados e testes unitários adicionados.

--- src/utils/CopyUtils.py
+++ src/utils/CopyUtils.py
@@ -3,14 +3,14 @@ from src.model.Subtitle import Subtitle
 
 
 def deep_copy_of_subs(subtitles: List[Subtitle]) -> List[Subtitle]:
-    subtitles_copied = []
+    subtitles_copy: List[Subtitle] = []
     for subtitle in subtitles:
-        subtitles_copied.append(_instance_subtitle(subtitle))
+        subtitles_copy.append(copy_subtitle(subtitle))
 
-    return subtitles_copied
+    return subtitles_copy
 
 
-def _instance_subtitle(subtitle_old: Subtitle) -> Subtitle:
+def copy_subtitle(subtitle_old: Subtitle) -> Subtitle:
     subtitle_new = Subtitle(subtitle_old.time_start, subtitle_old.time_end)
     subtitle_new.text = subtitle_old.text
     subtitle_new.audio = subtitle_old.audio
