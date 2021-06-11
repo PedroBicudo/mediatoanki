93ee6f4bc73dcd5ae4debdbff4b65b4b7a268970
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2021-06-11 14:30:48 -0300

    Métodos que validam o time_start com padding atualizados.

--- src/utils/TimeUtils.py
+++ src/utils/TimeUtils.py
@@ -17,21 +17,32 @@ class TimeUtils:
 
     def is_pad_start_invalid_with(self, seconds_added: timedelta) -> bool:
         return (
-            self.is_pad_negative_with(seconds_added) or
-            self.is_pad_start_more_than_pad_end_with(seconds_added)
+            self._is_time_start_negative_with(seconds_added) or
+            self._is_pad_start_more_than_pad_end_with(seconds_added)
         )
 
-    def is_pad_start_more_than_pad_end_with(self, seconds_added: timedelta) -> bool:
-        return self._time_start + seconds_added >= self._time_end
+    def _is_pad_start_more_than_pad_end_with(self, seconds_added: timedelta) -> bool:
+        time_start_new = self._time_start + seconds_added
+        return time_start_new >= self._time_end
 
     def is_pad_end_invalid_with(self, seconds_added: timedelta) -> bool:
         return (
-                self.is_pad_negative_with(seconds_added) or
+                self._is_time_end_negative_with(seconds_added) or
                 self.is_pad_end_less_than_pad_start_with(seconds_added)
         )
 
     def is_pad_end_less_than_pad_start_with(self, seconds_added: timedelta) -> bool:
         return self._time_end + seconds_added <= self._time_start
+
+    def _is_time_start_negative_with(self, seconds_added: timedelta):
+        return self._is_timedelta_negative_with(self._time_start, seconds_added)
+
+    def _is_time_end_negative_with(self, seconds_added: timedelta):
+        return self._is_timedelta_negative_with(self._time_end, seconds_added)
+
+    @staticmethod
+    def _is_timedelta_negative_with(time: timedelta, seconds_added: timedelta):
+        return time + seconds_added < timedelta(seconds=0)
 
     def is_pad_negative_with(self, seconds_added: timedelta) -> bool:
         return self._time_start + seconds_added < timedelta(seconds=0)
