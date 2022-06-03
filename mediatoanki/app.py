92bf5b2ad1d90b4824eb92a07ffd4edfc92745a6
Pedro <43938917+PedroBicudo@users.noreply.github.com>
2022-06-03 10:42:57 -0300

    chore: adiciona script poetry que roda o app.py (#34)

--- mediatoanki/app.py
+++ mediatoanki/app.py
@@ -2,38 +2,38 @@ import argparse
 
 from mediatoanki.MediaToAnkiArgParser import MediaToAnkiArgParser
 
-parser = argparse.ArgumentParser(description="Create an anki deck given media and subtitles.")
+parser = argparse.ArgumentParser(
+    description="Create an anki deck given media and subtitles."
+)
 
 parser.add_argument(
     "video_source",
     metavar="video_source",
     type=str,
-    help="Video file (.mp4, .mkv)"
+    help="Video file (.mp4, .mkv)",
 )
 parser.add_argument(
     "subtitle_source",
     metavar="subtitle_source",
-    type=str, help="Subtitle file (.vtt, .src, etc)"
+    type=str,
+    help="Subtitle file (.vtt, .src, etc)",
 )
 parser.add_argument(
     "--padstart",
     metavar="seconds",
     type=int,
     default=0,
-    help="Add more time to the start of the scene from subtitle"
+    help="Add more time to the start of the scene from subtitle",
 )
 parser.add_argument(
     "--padend",
     metavar="seconds",
     type=int,
     default=0,
-    help="Add more time to the end of the scene from subtitle"
+    help="Add more time to the end of the scene from subtitle",
 )
 parser.add_argument(
-    "destination",
-    metavar="dest",
-    type=str,
-    help="Destination for files."
+    "destination", metavar="dest", type=str, help="Destination for files."
 )
 
 
